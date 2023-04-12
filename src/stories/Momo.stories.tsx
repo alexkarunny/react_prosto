ef9610e7754353873abd902cc1fb242b62bdd8d2
alexkarunny <smetan4ik1@gmail.com>
2023-04-12 12:41:18 +0300

    07_reactMemo

---
+++ src/stories/Momo.stories.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'React memo demo',
+}
+
+
+const SecretCounter = (props: { num: number }) => {
+    console.log('Counter call')
+    return <div>Number - {props.num}</div>
+}
+
+const SecretUsers = (props: { users: string[] }) => {
+    console.log('Secret Users call')
+    console.log(props.users)
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(SecretUsers);
+const Counter = React.memo(SecretCounter)
+
+
+export const Memo = () => {
+    let [value, setValue] = useState<number>(1)
+    let [users, setUsers] = useState<string[]>(['Alex', 'Andrew'])
+
+    const addCounter = () => {
+        setValue(++value)
+    }
+    const addUser = () => {
+       setUsers([...users, `Sveta ${new Date().getTime()}`])
+    }
+    return <>
+        <button onClick={addCounter}>Add</button>
+        <button onClick={addUser}>AddUser</button>
+        <Counter num={value}/>
+        <Users users={users}/>
+    </>
+}
