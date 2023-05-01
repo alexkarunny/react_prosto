b6d894f08e2aa0263f76847df20f924ee16e822e
alexkarunny <smetan4ik1@gmail.com>
2023-05-01 15:42:18 +0300

    10_useEffect_clock

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
 export default {
     title: 'deep deep useState',
@@ -10,8 +10,6 @@ function generateData() {
 }
 
 export const NewUseState = () => {
-    console.log('state')
-
 
     let [counter, setCounter] = useState<number>(generateData) // запихнуть в инит значение функцию, и она вызовется только один раз для расчета инит значения; ф изменения стейта можно запихнуть ф
 
@@ -19,10 +17,45 @@ export const NewUseState = () => {
         return state + 1
     }
 
+useEffect (() => {
+    console.log('useEffect every render')
+    document.title = counter.toString()
+})
+useEffect (() => {
+    console.log('useEffect only first render === componentDidMount')
+    document.title = counter.toString()
+}, [])
+useEffect (() => {
+    console.log('useEffect first render and every time then counter changing == componentDidUpdate')
+    document.title = counter.toString()
+}, [counter])
+
     return (
         <div>
             <button onClick={() => setCounter(changer)}>++</button>
             {counter}
+        </div>
+    )
+}
+
+export const SetTimeoutExample = () => {
+
+
+    let [date, setDate] = useState<Date>() // запихнуть в инит значение функцию, и она вызовется только один раз для расчета инит значения; ф изменения стейта можно запихнуть ф
+    useEffect(() => {
+        setInterval(() => {
+            const date = new Date()
+            setDate(date)
+
+        }, 1000)
+    }, [])
+
+    let hours = date?.getHours()
+    let minutes = date?.getMinutes()
+    let seconds = date?.getSeconds()
+    return (
+        <div>
+            {`${hours}:${minutes}:${seconds}`}
         </div>
     )
 }
